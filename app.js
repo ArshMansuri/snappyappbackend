253dfd7170735898d947f7a297f84cb29d13fd34
MansuriArsh <mansuriarsh44@gmail.com>
2023-12-27 20:58:25 +0530

    cors fix 2

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ connectDataBase();
 
 
 //--------------- middleware--------------
-app.use(cookiParser());
-app.use(express.json({limit: "50mb"}));
-app.use(express.urlencoded({extended: true}));
-app.use("*", cors({
+app.use(cors({
     origin: true,
     credentials: true
 }))
+app.use(cookiParser());
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({extended: true}));
 
 
 //---------------import router ----------
